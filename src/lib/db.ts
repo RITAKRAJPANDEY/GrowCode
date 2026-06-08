c0eee51e275111e54a5e7d64519091c9e91144f8
RITAKRAJPANDEY <rp2194679@gmail.com>
2026-06-08 14:03:58 +0530

    added zod validation for env variables

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
 import {Pool} from 'pg';
-
+import {env} from './env';// uses the zod validation in the env.ts
 // so that it dosen't hot reloads everytime we rerender and pg reaches pool limit in dev
 declare global{
     var postgresPool:Pool |undefined;
@@ -10,20 +10,20 @@ let pool : Pool;
 
 if(process.env.NODE_ENV==='production'){
      pool = new Pool ({
-        user:process.env.DB_USER,
-        host:process.env.DB_HOST,
-        database:process.env.DB_NAME,
-        port:process.env.DB_PORT?Number(process.env.DB_PORT):5432,
-        password:process.env.DB_PASSWORD
+        user:env.DB_USER,
+        host:env.DB_HOST,
+        database:env.DB_NAME,
+        port:env.DB_PORT,
+        password:env.DB_PASSWORD
     });
 }else{// if it's dev and no pool exists declare the global var postgresPool 
     if(!globalThis.postgresPool){
         globalThis.postgresPool = new Pool ({
-        user:process.env.DB_USER,
-        host:process.env.DB_HOST,
-        database:process.env.DB_NAME,
-        port:process.env.DB_PORT?Number(process.env.DB_PORT):5432,
-        password:process.env.DB_PASSWORD,
+        user:env.DB_USER,
+        host:env.DB_HOST,
+        database:env.DB_NAME,
+        port:env.DB_PORT?Number(process.env.DB_PORT):5432,
+        password:env.DB_PASSWORD,
         });
 }// if it already exists let pool be that global var so we don't create a new pool 
  pool = globalThis.postgresPool;
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,27 @@
 import z from "zod";
 
-export const validateEnvSchema = z.object({
+const validateEnvSchema = z.object({
     NODE_ENV:z.enum(['production','test','development']).default('development'),
-    
+    DB_USER:z.string().min(1,"Database user is required"),
+    DB_PASSWORD:z.string().min(1,"Database password is required"),
+    DB_NAME:z.string().min(1,"Database name is requried"),
+    DB_HOST:z.string().min(1,"Database host is requred"),
+    DB_PORT:z.coerce.number().default(5432)// coerce forces the string into a number js object (document se nikala )
+
 });
+
+const parsedEnv = validateEnvSchema.safeParse(process.env);
+
+if(!parsedEnv.success){
+    console.error("Invalid environment variables ",parsedEnv.error.format());
+    throw new Error("Invalid environment variables");
+}
+
+export const env = parsedEnv.data;
+
+
+// DB_USER=
+// DB_PASSWORD=
+// DB_PORT=
+// DB_NAME=
+// DB_HOST=
