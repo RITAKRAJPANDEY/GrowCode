9ca6d647ab7b9bd931584b0d8a505d6c90134d7a
RITAKRAJPANDEY <rp2194679@gmail.com>
2026-07-05 17:39:59 +0530

    added chat and stats svg fixed navbar bug

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -26,10 +26,15 @@ export default function Navbar() {
 
 
             <div className="bg-[#020617] rounded-md">
-                <h1 className="text-3xl font-bold px-2">
+                <div className="flex flex-row   items-center justify-center">
+                     <h1 className="text-3xl font-bold px-2">
                     <span className="text-[#34d399]">
                         Grow
                     </span>Code</h1>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="M2 13a2 2 0 0 0 2-2V7a2 2 0 0 1 4 0v13a2 2 0 0 0 4 0V4a2 2 0 0 1 4 0v13a2 2 0 0 0 4 0v-4a2 2 0 0 1 2-2"/></svg>
+                    
+                </div>
+               
             </div>
             <div >
                 <div className="flex flex-col gap-4">
@@ -49,33 +54,39 @@ export default function Navbar() {
 
                 <div className="flex flex-col gap-8">
 
-                    <div className="flex flex-col gap-2">
+                    <div className="flex flex-col gap-2 ">
+                        <div className="flex flex-row gap-2 justify bg-[#0f172a]/30 rounded">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="M3 11h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-5Zm0 0a9 9 0 1 1 18 0m0 0v5a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3Z"/><path d="M21 16v2a4 4 0 0 1-4 4h-5"/></svg>
+                            <h2 className="py-1 px-2 text-sm font-semibold uppercase tracking-wider text-[#34d399] rounded">
+                                Chat
+                            </h2>
+                        </div>
 
-                        <h2 className="py-1 px-2 text-sm font-semibold uppercase tracking-wider text-[#34d399] bg-[#0f172a]/30 rounded">
-                            Chat
-                        </h2>
                         <Link href="/chat" className={` ${pathname === "/chat" ? "text-green-300 " : "text-slate-400"} flex py-2 justify-center hover:bg-[#0f172a] hover:scale-98 rounded-md w-full text-xl hover:text-green-300 transition-all `}>Group</Link>
                         {/* <Link href="/chat?username=user1" className="flex py-1.5 justify-center hover:bg-[#0f172a] hover:scale-98 rounded-md w-full text-lg hover:text-white text-slate-400">user1</Link>
                     <Link href="/" className="flex py-1.5 justify-center hover:bg-[#0f172a] hover:scale-98 rounded-md w-full text-lg hover:text-white text-slate-400">user2</Link> */}
                     </div>
 
                     <div className="flex flex-col gap-2">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="w-6 h-6 text-white"
-                        >
-                            <path d="M3 3v16a2 2 0 0 0 2 2h16" />
-                            <path d="m19 9-5 5-4-4-3 3" />
-                        </svg>
-                        <h2 className="py-1 px-2 text-sm font-semibold uppercase tracking-wider text-[#34d399] bg-[#0f172a]/30 rounded">
-                            Statistics
-                        </h2>
+                        <div className="flex flex-row items-center gap-2 bg-[#0f172a]/30 rounded">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="w-6 h-6 text-white"
+                            >
+                                <path d="M3 3v16a2 2 0 0 0 2 2h16" />
+                                <path d="m19 9-5 5-4-4-3 3" />
+                            </svg>
+                            <h2 className="py-1 px-2 text-sm font-semibold uppercase tracking-wider text-[#34d399] ">
+                                Statistics
+                            </h2>
+                        </div>
+
                         <Link href="/" className="flex py-1.5 justify-center hover:bg-[#0f172a] hover:scale-98 rounded-md w-full text-lg hover:text-red-700 text-slate-400">Be Content</Link>
                         <Link href="/" className="flex py-1.5 justify-center hover:bg-[#0f172a] hover:scale-98 rounded-md w-full text-lg hover:text-green-300 text-slate-400">Be Competent</Link>
                     </div>
